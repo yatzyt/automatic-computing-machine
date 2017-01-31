251e6883707d864b655dc11dbdda380e9f2c2fa9
Andrew <antongyang@yahoo.com>
2017-01-31 11:34:00 -0700

    updated to included pandas

--- parse.py
+++ parse.py
@@ -1,33 +1,53 @@
 import pandas as pd
+from itertools import (takewhile,repeat)
+
+def rawbigcount(filename):
+    f = open(filename, 'r')
+    bufgen = takewhile(lambda x: x, (f.read(1024*1024) for _ in repeat(None)))
+    return sum( buf.count(b'\n') for buf in bufgen if buf )
+
+def percentage(up, down):
+    print float(up)/float(down)*100, '%'
 
 fname = "03496-0001-Data.txt"
+
+total = rawbigcount(fname)
 
 file = open(fname, "r")
 
 content = file.readlines()
 content = [x.rstrip("\n") for x in content]
 
-parsed = []
+parsed = pd.DataFrame({ 'Date of Birth':[], 'Gender':[], 'Race':[] ,'Current Status Supervision Status':[], '# Priors':[], 'Drug Type':[], 'Drug Amount':[], 'Statutory Offense Grade':[], 'Type of Incarceration':[], 'Length of Incarceration':[], 'Restitution Amount':[], 'Height/Weight':[], 'Armed Career Criminal Status Applied':[], 'Career Offender Status Applied':[], 'Citizenship':[], 'Education':[], 'Marital Status':[], 'Previous Sentencing Time':[], 'Cost of Bail':[], 'Warrants':[], 'District':[] })
+
+check = 1
 
 for x in content:
     y = {}
-    y.update({'Date of Birth': x[489:497]})  # DDMMYYYY, 1012001 = Missing
-    y.update({'Gender': x[497:498]})         # 0 = Male, 1 = Female
-    y.update({'Race': x[488:489]})           # 1 = White/Caucasian, 2 = Black, 3 = American Indian/Alaska Native
-                                             # 4 = Asian/Pacific Islander, 5 = Mutil-racial, 6 = Other
+    y.update({'Date of Birth': x[489:497]})
+    y.update({'Gender': x[497:498]})
+    y.update({'Race': x[488:489]})
+    y.update({'Current Status Supervision Status': []})
+    y.update({'# Priors': []})
+    y.update({'Drug Type': []})
+    y.update({'Drug Amount': []})
+    y.update({'Statutory Offense Grade': []})
+    y.update({'Type of Incarceration': []})
+    y.update({'Length of Incarceration': []})
+    y.update({'Restitution Amount': []})
+    y.update({'Height/Weight': []})
     y.update({'Armed Career Criminal Status': x[417:418]})
-                                             # 0 = Not applied, 1 = applied
     y.update({'Career Offender Status': x[418:419]})
-                                             # 0 = Not applied, 1 = applied
-    y.update({'Citizenship': x[419:422]})    # Check booklet aka add later
-    y.update({'Highest edu': x[435:437]})    # 0 = None, 1-11 = number of years completed, 12 = High school
-                                             # 13-15 = years in college, 16 = college grad, 21 = GED
-                                             # 22 = Trade/vocational deg, 23 = associate, 24 = Grad
-                                             # 31 = Some elementary, 32 = some high, 33 = some trade/vocational
-                                             # 34 = some college, 35 = some grad, 36 = military training, 37 = Middle/jr high
-    y.update({'Marital Status': x[446:447]}) # 1 = single, 2 = married, 3 = Cohabitating, 4 = divorced, 5 = widowed, 6 = separated
-    y.update({'District': x[402:404]})       # as listed in reports
+    y.update({'Citizenship': x[419:422]})
+    y.update({'Education': x[435:437]})
+    y.update({'Marital Status': x[446:447]})
+    y.update({'Previous Sentencing Time': []})
+    y.update({'Cost of Bail': []})
+    y.update({'Warrants': []})
+    y.update({'District': x[402:404]})
+    parsed.loc[-1] = y
+    parsed.index = parsed.index + 1
+    print percentage(check, total)
+    check = check + 1
 
-    parsed.append(y)
-
-print (y)
+print parsed.loc[0]
